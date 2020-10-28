53c23de18445be3212e2ff90c309a58edd6b6521
Luke Pajer <31792929+The-Geology-Guy@users.noreply.github.com>
2020-10-28 11:25:37 -0400

    Update __init__.py

--- GPS_Strain/__init__.py
+++ GPS_Strain/__init__.py
@@ -1,1 +1,12 @@
+from .version import __version__
+from .GPS_Strain import unavco_data
+from .GPS_Strain import strain_data
+from .GPS_Strain import strain_viz
 
+# if somebody does "from somepackage import *", this is what they will
+# be able to access:
+__all__ = [
+    'unavco_data',
+    'strain_data',
+    'strain_viz',
+]
